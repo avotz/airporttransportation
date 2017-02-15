624aab5b9bafe3fb27a9521b5faf4c05970cea8a
Alonso Chavarria Cubero <alons182@hotmail.com>
2017-02-15 17:17:21 -0600

    fix readmore

--- wp-content/themes/airport/template-parts/content.php
+++ wp-content/themes/airport/template-parts/content.php
@@ -42,7 +42,7 @@
 		<?php
 			the_content( sprintf(
 				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'airport' ), array( 'span' => array( 'class' => array() ) ) ),
+				wp_kses( __( 'Read More <span class="meta-nav">&rarr;</span>', 'airport' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
 
