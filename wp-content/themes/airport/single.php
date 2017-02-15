d8f67a4c086d6851859b2156f4d4d08446497d6f
Alonso Chavarria Cubero <alons182@hotmail.com>
2017-02-15 16:19:32 -0600

    se cambio blog style

--- wp-content/themes/airport/single.php
+++ wp-content/themes/airport/single.php
@@ -10,7 +10,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<div class="inner" >
 
 		<?php
 		while ( have_posts() ) : the_post();
@@ -27,7 +27,7 @@ get_header(); ?>
 		endwhile; // End of the loop.
 		?>
 
-		</main><!-- #main -->
+		</div><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
